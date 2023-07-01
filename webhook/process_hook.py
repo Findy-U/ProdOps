d078e0688561f4486ef39c0063bd3a5e964e67e3
Demian143 <patrickxavier140@gmail.com>
2023-06-30 23:15:32 -0300

    Removing created_at block from assignee block

--- webhook/process_hook.py
+++ webhook/process_hook.py
@@ -36,54 +36,55 @@ def process_webhook(data):
                     login = assignee.get('login')
                     assignee_login = login if login else None
 
-                created_at = issue.get('created_at')
+            # Created at block
+            created_at = issue.get('created_at')
 
-                if created_at is not None:
-                    created_at = datetime.strptime(
-                        created_at, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
+            if created_at:
+                created_at = datetime.strptime(
+                    created_at, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
 
-                closed_at = issue.get('closed_at')
+            closed_at = issue.get('closed_at')
 
+            if closed_at:
+                closed_at = datetime.strptime(
+                    closed_at, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
+
+            status = 'open' if closed_at is None else 'closed'
+
+            existing_data = session.query(Alldata).filter_by(
+                project_card_id=project_card_id).first()
+
+            if existing_data:
+                if assignee_login is not None:
+                    existing_data.assignee = assignee_login
                 if closed_at is not None:
-                    closed_at = datetime.strptime(
-                        closed_at, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
-
-                status = 'open' if closed_at is None else 'closed'
-
-                existing_data = session.query(Alldata).filter_by(
-                    project_card_id=project_card_id).first()
-
-                if existing_data:
-                    if assignee_login is not None:
-                        existing_data.assignee = assignee_login
-                    if closed_at is not None:
-                        existing_data.closed_at = closed_at
-                    if created_at is not None:
-                        existing_data.created_at = created_at
-                    if status is not None:
-                        existing_data.status = status
-                    try:
-                        session.commit()
-                        logger.info(
-                            'Data updated successfully for record_id: %s', existing_data.record_id)
-                    except Exception as e:
-                        logger.error(
-                            "An error occurred during updating data: %s", str(e))
-                        session.rollback()
-                else:
-                    logger.info(
-                        'No existing data found for project_card_id: %s', project_card_id)
-                    all_data = Alldata(
-                        project_card_id=project_card_id,
-                        created_at=created_at,
-                        closed_at=closed_at,
-                        status=status,
-                        assignee=assignee_login
-                    )
-                    session.add(all_data)
+                    existing_data.closed_at = closed_at
+                if created_at is not None:
+                    existing_data.created_at = created_at
+                if status is not None:
+                    existing_data.status = status
+                try:
                     session.commit()
                     logger.info(
-                        'Data saved successfully with record_id: %s', all_data.record_id)
+                        'Data updated successfully for record_id: %s', existing_data.record_id)
+                except Exception as e:
+                    logger.error(
+                        "An error occurred during updating data: %s", str(e))
+                    session.rollback()
+            else:
+                logger.info(
+                    'No existing data found for project_card_id: %s', project_card_id)
+                all_data = Alldata(
+                    project_card_id=project_card_id,
+                    created_at=created_at,
+                    closed_at=closed_at,
+                    status=status,
+                    assignee=assignee_login
+                )
+                session.add(all_data)
+                session.commit()
+                logger.info(
+                    'Data saved successfully with record_id: %s', all_data.record_id)
 
         else:
             logger.error(
