167de32df5d6c8ae49810cb7b86d435001fa5abc
Demian143 <patrickxavier140@gmail.com>
2023-07-04 21:33:17 -0300

    Isolating parse_issue to a new file and reducing try except

--- webhook/process_hook.py
+++ webhook/process_hook.py
@@ -1,7 +1,7 @@
-from datetime import datetime, timezone
 from models.models import Alldata
 from logger.logger import logger
 from config import session as Session
+from parse_issue import parse_issue
 
 # Logger
 logger = logger()
@@ -22,23 +22,22 @@ def process_webhook(data: dict):
             return
 
         if action == 'reopened':
-            # 1 - Get object by id
             parsed_issue = parse_issue(issue)
             project_card_id = parsed_issue['project_card_id']
 
             existing_data = session.query(Alldata).filter_by(
                 project_card_id=project_card_id).first()
 
-            # 2 - Set closed_at to None and status to Open
             existing_data.closed_at = None
             existing_data.status = 'Maintenance'
 
             session.add(existing_data)
             session.commit()
+            session.close()
+
             logger.info(
                 'Issue reopened successfully for record_id: %s',
                 existing_data.record_id)
-            # 3.1 - return response
             return
 
         if action in ['opened', 'assigned', 'closed', 'reordered', 'edited']:
@@ -51,7 +50,6 @@ def process_webhook(data: dict):
 
             status = 'open' if closed_at is None else 'closed'
 
-            # Check if record is already in DB
             existing_data = session.query(Alldata).filter_by(
                 project_card_id=project_card_id).first()
 
@@ -65,19 +63,19 @@ def process_webhook(data: dict):
                 if status:
                     existing_data.status = status
 
-                try:
-                    session.commit()
-                    logger.info(
-                        'Data updated successfully for record_id: %s', existing_data.record_id)
+                session.commit()
+                session.close()
 
-                except Exception as e:
-                    logger.error(
-                        "An error occurred during updating data: %s", str(e))
-                    session.rollback()
+                logger.info(
+                    'Data updated successfully for record_id: %s',
+                    existing_data.record_id)
+                return
 
             else:
                 logger.info(
-                    'No existing data found for project_card_id: %s', project_card_id)
+                    'No existing data found for project_card_id: %s',
+                    project_card_id)
+
                 all_data = Alldata(
                     project_card_id=project_card_id,
                     created_at=created_at,
@@ -87,9 +85,13 @@ def process_webhook(data: dict):
                 )
                 session.add(all_data)
                 session.commit()
-                logger.info(
-                    'Data saved successfully with record_id: %s', all_data.record_id)
+                session.close()
 
+                logger.info(
+                    'Data saved successfully with record_id: %s',
+                    all_data.record_id)
+
+                return
         else:
             logger.error(
                 "Unsupported action: %s. Skipping webhook processing.", action)
@@ -98,40 +100,3 @@ def process_webhook(data: dict):
         session.rollback()
     finally:
         session.close()
-
-
-def parse_issue(issue: dict) -> dict:
-    """ This function separates the parsing of issue dictionary from
-        the rest of the webhook code, making it easier to read. """
-
-    project_card_id = str(issue.get('id'))
-    assignee = issue.get('assignee')
-    assignee_login = None
-
-    if not assignee:
-        assignees = issue.get('assignees')
-
-        if assignees:
-            assignee = assignees[0]
-            login = assignee.get('login')
-            assignee_login = login if login else None
-
-    created_at = issue.get('created_at')
-
-    if created_at:
-        created_at = datetime.strptime(
-            created_at, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
-
-    closed_at = issue.get('closed_at')
-
-    if closed_at:
-        closed_at = datetime.strptime(
-            closed_at, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
-
-    return {
-        "project_card_id": project_card_id,
-        "assignee": assignee,
-        "assignee_login": assignee_login,
-        "created_at": created_at,
-        "closed_at": closed_at
-    }
