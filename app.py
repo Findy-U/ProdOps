cc3f830c552cd1a574ec5584295080682a489ea1
Demian143 <patrickxavier140@gmail.com>
2023-07-11 21:48:00 -0300

    Removing references to declarative_base and config file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from models.models import db, Base, Alldata
+from models.models import db, Alldata
 from logger.logger import logger
-from config import engine
+import dotenv
 import os
 
 from routes.webhook import webhook_route
@@ -11,6 +11,9 @@ logger = logger()
 
 
 def create_app(test: bool = False) -> Flask:
+    # Get all env variables
+    dotenv.load_dotenv('.env')
+
     app = Flask(__name__)
 
     if test:
@@ -23,17 +26,19 @@ def create_app(test: bool = False) -> Flask:
         db.init_app(app)
 
         with app.app_context():
-            Base.metadata.create_all(engine)
+            db.create_all()
 
-        return register_routes(app)
+        register_routes(app)
+
+        return app
 
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
         'SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-
-    return register_routes(app)
+    register_routes(app)
+    return app
 
 
 def register_routes(app: Flask) -> Flask:
@@ -47,7 +52,7 @@ app = create_app()
 if __name__ == "__main__":
     with app.app_context():
         try:
-            Base.metadata.create_all(engine)
+            db.create_all()
             logger.info("Database connected and tables created successfully.")
         except Exception as e:
             logger.error(f"Error setting up database: {e}", exc_info=True)
