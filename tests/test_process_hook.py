4bc3e8631ff6cbf6917e7b197ec77cd547664814
Demian143 <patrickxavier140@gmail.com>
2023-07-11 21:30:02 -0300

    Fixing test passing str intead of dict payload

--- tests/test_process_hook.py
+++ tests/test_process_hook.py
@@ -1,7 +1,6 @@
 from payload_example.full_payload_example import full_payload
-import json
 
 
 def test_send_payload_to_webhook(client) -> None:
-    response = client.post('/payload', json=json.dumps(full_payload))
+    response = client.post('/payload', json=full_payload)
     assert response.status_code == 200
