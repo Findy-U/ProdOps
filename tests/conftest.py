92e1b899585540644a713928a7fb95ff880ac780
Demian143 <patrickxavier140@gmail.com>
2023-07-13 19:33:27 -0300

    Adding teardown

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,22 @@
 import pytest
 from app import create_app
+from models.models import TestDB, db
 
 
 @pytest.fixture()
 def app():
+    # Setup
     app = create_app(True)
     app.config.update({
         "TESTING": True,
     })
 
     yield app
+
+    # Teardown
+    with app.app_context():
+        # Clear all records from TestDB
+        db.session.query(TestDB).delete()
 
 
 @pytest.fixture()
