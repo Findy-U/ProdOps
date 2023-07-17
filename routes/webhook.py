2e05165ee212ce2f6c3f84d58d39d3ab6dda3e7c
Demian143 <patrickxavier140@gmail.com>
2023-07-17 00:39:37 -0300

    Fixing error that lead the webhook route to return status code 200 for any action

--- routes/webhook.py
+++ routes/webhook.py
@@ -1,4 +1,4 @@
-from flask import request, abort, jsonify, Blueprint
+from flask import request, abort, Blueprint, Response
 
 from logger.logger import logger
 from webhook.process_hook import process_webhook
@@ -8,13 +8,12 @@ webhook_route = Blueprint('webhook', __name__)
 
 
 @webhook_route.route('/payload', methods=['POST'])
-def webhook():
+def webhook() -> Response:
     if not request.is_json:
         logger.error('Invalid JSON payload')
         abort(400)
 
     data = request.get_json()
     # logger.info('Received webhook payload: %s', data)
-    process_webhook(data)
 
-    return jsonify({'message': 'Webhook processed successfully'})
+    return process_webhook(data)
