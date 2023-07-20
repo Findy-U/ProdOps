f87e1e4ad5fbf15227d8c8c0bae277d233474f6c
Demian143 <patrickxavier140@gmail.com>
2023-07-20 14:13:35 -0300

    Adding and refactoring tests

--- tests/test_models/test_cards_models.py
+++ tests/test_models/test_cards_models.py
@@ -1,24 +1,23 @@
-from models.cards import Cards
+from models.cards import CardAssignees
 from models.models import db
 
 
-def test_create_new_card(app):
-    with app.app_context():
-        new_card = Cards(
-            card_name='Testing Cards',
-            card_description='Lorem Ipsum...')
-
-        db.session.add(new_card)
-        db.session.commit()
-
-        assert type(new_card.card_id) is int
-
-        # Delete Test Record
-        db.session.delete(new_card)
-        db.session.commit()
+def test_create_generic_card(generic_card):
+    assert type(generic_card.card_id) is int
 
 
-"""
-def test_add_assignee_to_card():
-    return
-"""
+def test_add_assignee_to_card(generic_card):
+    assignee = CardAssignees(
+        assignee_name='Demian143',
+        card_id=generic_card.card_id
+    )
+
+    db.session.add(assignee)
+    db.session.commit()
+
+    assert assignee.card_id == generic_card.card_id
+    assert generic_card.card_assignees[0].assignee_id == assignee.assignee_id
+
+    # Teardown
+    db.session.delete(assignee)
+    db.session.commit()
