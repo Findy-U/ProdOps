d24668cea34209e4e54744134937025d6d70b7c8
Demian143 <patrickxavier140@gmail.com>
2023-07-25 21:55:32 -0300

    Fixing wrong references

--- models/cards.py
+++ models/cards.py
@@ -15,21 +15,30 @@ class Card(db.Model):
     card_description = db.Column(db.Text)
     # Relationships
     card_movements = db.relationship(
-        'CardMovements', backref='cards', lazy=True)
+        'CardMovement', backref='card', lazy=True)
     card_assignees = db.relationship(
-        'CardAssignees', backref='cards', lazy=True)
+        'CardAssignee', backref='card', lazy=True)
+
     # Many-to-Many ref: https://docs.sqlalchemy.org/en/20/orm/basic_relationships.html#many-to-many
-    card_label = db.relationship(
+    labels = db.relationship(
         'Label',
         secondary=card_labels,
         lazy='subquery',
-        backref=db.backref('cards', lazy=True))
+        back_populates='cards')
 
 
 class Label(db.Model):
     label_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     label_name = db.Column(db.String(200), nullable=False)
     label_date_time = db.Column(db.DateTime)
+
+    # Many-to-Many ref: https://docs.sqlalchemy.org/en/20/orm/basic_relationships.html#many-to-many
+    cards = db.relationship(
+        'Card',
+        secondary=card_labels,
+        lazy='subquery',
+        back_populates='labels'
+    )
 
 
 class CardMovement(db.Model):
@@ -39,7 +48,7 @@ class CardMovement(db.Model):
     movement_date_time = db.Column(db.DateTime)
     # Foreign Key
     card_id = db.Column(db.Integer, db.ForeignKey(
-        'cards.card_id'))
+        'card.card_id'))
 
 
 class CardAssignee(db.Model):
@@ -48,4 +57,4 @@ class CardAssignee(db.Model):
     assignee_date_time = db.Column(db.DateTime)
     # Foreign Key
     card_id = db.Column(db.Integer, db.ForeignKey(
-        'cards.card_id'))
+        'card.card_id'))
