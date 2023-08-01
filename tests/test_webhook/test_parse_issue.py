0394b17ed85aa19261b5516e4da065015e3de713
Demian143 <patrickxavier140@gmail.com>
2023-08-01 12:27:27 -0300

    Linting test_parse_issue

--- tests/test_webhook/test_parse_issue.py
+++ tests/test_webhook/test_parse_issue.py
@@ -10,9 +10,17 @@ def test_parse_issue_returns_dict() -> None:
 
 def test_parse_issue_returns_expected_dict() -> None:
     expected_return = {'project_card_id': '1793822382',
-                       'assignee': {'login': 'Demian143', 'id': 81446007,
-                                    'node_id': 'MDQ6VXNlcjgxNDQ2MDA3', 'avatar_url': 'https://avatars.githubusercontent.com/u/81446007?v=4', 'gravatar_id': '', 'url': 'https://api.github.com/users/Demian143', 'html_url': 'https://github.com/Demian143', 'followers_url': 'https://api.github.com/users/Demian143/followers', 'following_url': 'https://api.github.com/users/Demian143/following{/other_user}', 'gists_url': 'https://api.github.com/users/Demian143/gists{/gist_id}', 'starred_url': 'https://api.github.com/users/Demian143/starred{/owner}{/repo}',
-                                    'subscriptions_url': 'https://api.github.com/users/Demian143/subscriptions', 'organizations_url': 'https://api.github.com/users/Demian143/orgs', 'repos_url': 'https://api.github.com/users/Demian143/repos', 'events_url': 'https://api.github.com/users/Demian143/events{/privacy}', 'received_events_url': 'https://api.github.com/users/Demian143/received_events', 'type': 'User', 'site_admin': False},
+                       'assignee': {
+                           'login': 'Demian143', 'id': 81446007,
+                           'node_id': 'MDQ6VXNlcjgxNDQ2MDA3',
+                           'avatar_url': 'https://avatars.githubusercontent.com/u/81446007?v=4',
+                           'gravatar_id': '', 'url': 'https://api.github.com/users/Demian143',
+                           'html_url': 'https://github.com/Demian143',
+                           'followers_url': 'https://api.github.com/users/Demian143/followers',
+                           'following_url': 'https://api.github.com/users/Demian143/following{/other_user}',
+                           'gists_url': 'https://api.github.com/users/Demian143/gists{/gist_id}',
+                           'starred_url': 'https://api.github.com/users/Demian143/starred{/owner}{/repo}',
+                           'subscriptions_url': 'https://api.github.com/users/Demian143/subscriptions', 'organizations_url': 'https://api.github.com/users/Demian143/orgs', 'repos_url': 'https://api.github.com/users/Demian143/repos', 'events_url': 'https://api.github.com/users/Demian143/events{/privacy}', 'received_events_url': 'https://api.github.com/users/Demian143/received_events', 'type': 'User', 'site_admin': False},
                        'assignee_login': None,
                        'created_at': datetime.datetime(2023, 7, 7, 16, 24, 5, tzinfo=datetime.timezone.utc),
                        'closed_at': None,
