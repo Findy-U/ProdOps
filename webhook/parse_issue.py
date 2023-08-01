b5eb5361325192f9db4ed719120536946ee1913a
Demian143 <patrickxavier140@gmail.com>
2023-08-01 12:24:23 -0300

    Removing 'if created_at:' because create_at should never be none.

--- webhook/parse_issue.py
+++ webhook/parse_issue.py
@@ -1,16 +1,3 @@
-# Este código define uma função, parse_issue(), que analisa uma "issue"
-# (problema) de um sistema de gerenciamento de projetos. A função
-# recebe um dicionário representando a "issue" como entrada e retorna
-# um novo dicionário contendo informações analisadas da "issue",
-# incluindo o ID do projeto, o responsável, a data de criação e a data
-# de encerramento.
-
-# Durante a análise, o código também verifica se certos campos estão
-# presentes na "issue". Se um campo necessário (como o ID da "issue"
-# ou a data de criação) estiver ausente, a função levanta uma exceção.
-# Se um campo opcional (como o responsável ou a data de fechamento)
-# estiver ausente, a função simplesmente emite um aviso e continua.
-
 from datetime import datetime, timezone
 
 
@@ -31,12 +18,6 @@ def parse_issue(issue: dict) -> dict:
             login = assignee.get('login')
             assignee_login = login if login else None
 
-    created_at = issue.get('created_at')
-
-    if created_at:
-        created_at = datetime.strptime(
-            created_at, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
-
     closed_at = issue.get('closed_at')
 
     if closed_at:
@@ -44,6 +25,10 @@ def parse_issue(issue: dict) -> dict:
             closed_at, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
 
     repository_url = issue.get('repository_url')
+
+    created_at = datetime.strptime(
+        issue.get('created_at'),
+        '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=timezone.utc)
 
     return {
         "project_card_id": project_card_id,
